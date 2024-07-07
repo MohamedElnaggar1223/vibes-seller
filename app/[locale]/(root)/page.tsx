0128e50255ab5d8da902ab2e46b9d154a9fedbd4
mohamedelnaggar1223 <maelnaggar1223@gmail.com>
2024-07-07 13:03:11 +0300

    First

--- app/[locale]/(root)/page.tsx
+++ app/[locale]/(root)/page.tsx
@@ -9,15 +9,13 @@ import { Suspense } from "react";
 
 type Props = {
 	params: {
-		locale?: string
+		locale?: string | undefined
 	}
 	searchParams: { [key: string]: string | string[] | undefined }
 }
 
 export default async function Home({ searchParams, params }: Props)
 {
-	const admin = await initAdmin()
-
 	const categories = await getCategories()
 	const events = await getEvents()
 
@@ -28,11 +26,11 @@ export default async function Home({ searchParams, params }: Props)
 	
 	return (
 		<section className='flex flex-col items-center justify-center flex-1 gap-8 overflow-x-hidden px-6 md:px-20' key={Math.random()}>
-			<SearchBar locale={params.locale} />
+			<SearchBar locale={params?.locale} />
 			{
 				(search || date || country || category) ? (
 					<Suspense fallback={<SearchLoading />}>
-						<Search locale={params.locale} search={search} date={date} category={category} country={country} categories={categories} />
+						<Search locale={params?.locale} search={search} date={date} category={category} country={country} categories={categories} />
 					</Suspense>
 				) : (
 					<div className='flex items-start justify-start mb-auto flex-wrap gap-8'>
