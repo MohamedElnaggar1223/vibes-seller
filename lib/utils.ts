a1d09e6b0e53d66df8eb7c74f84ea9d2a2227fdd
MohamedElnaggar1223 <maelnaggar1223@gmail.com>
2024-09-09 18:44:31 +0300

    Event Time Fix

--- lib/utils.ts
+++ lib/utils.ts
@@ -66,7 +66,7 @@ export const getEvents = cache(async () => {
         return {
             ...event.data(),
             createdAt: event.data()?.createdAt?.toDate(),
-            eventTime: event.data()??.eventTime?.toDate(),
+            eventTime: event.data()?.eventTime?.toDate(),
             eventDate: event.data()?.eventDate?.toDate(),
             updatedAt: event.data()?.updatedAt?.toDate(),
         } as EventType
@@ -85,7 +85,7 @@ export const getEvent = cache(async (id: string) => {
     return {
         ...event.data(),
         createdAt: event.data()?.createdAt.toDate(),
-        eventTime: event.data()??.eventTime.toDate(),
+        eventTime: event.data()?.eventTime.toDate(),
         eventDate: event.data()?.eventDate.toDate(),
         updatedAt: event.data()?.updatedAt?.toDate(),
         gatesClose: event.data()?.gatesClose?.toDate(),
