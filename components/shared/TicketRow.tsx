1c38f5c142319a700fa31c7ab856931da679f948
MohamedElnaggar1223 <maelnaggar1223@gmail.com>
2024-09-19 18:59:45 +0300

    Seller

--- components/shared/TicketRow.tsx
+++ components/shared/TicketRow.tsx
@@ -106,14 +106,14 @@ export default function TicketRow({ event, ticket, bundleTickets }: Props)
                                 className='object-cover w-[62px] h-[60px] rounded-lg' 
                                 quality={100}
                             />
-                            <p className={cn('font-medium text-black h-full flex-1 text-center flex items-center justify-center', ticket.saleStatus === 'sold' ? 'bg-[#A3CCA9]' : 'bg-[#CCCCCC]')}>{event?.name}</p> 
+                            <p className={cn('font-medium text-black h-full flex-1 text-center flex items-center justify-center', (ticket as TicketType).saleStatus === 'sold' ? 'bg-[#A3CCA9]' : 'bg-[#CCCCCC]')}>{event?.name}</p> 
                         </div>
-                        <div className={cn('flex-1 h-full flex items-center justify-center font-medium text-xs text-black text-center font-poppins', ticket.saleStatus === 'sold' ? 'bg-[#CCF5D2]' : 'bg-[#fff]')}>{Object.keys(ticket.tickets)[0]}</div>
+                        <div className={cn('flex-1 h-full flex items-center justify-center font-medium text-xs text-black text-center font-poppins', ticket.saleStatus === 'sold' ? 'bg-[#CCF5D2]' : 'bg-[#fff]')}>{Object.keys((ticket as TicketType).tickets)[0]}</div>
                         <div className={cn('flex-1 h-full flex items-center justify-center font-medium text-xs text-black text-center font-poppins', ticket.saleStatus === 'sold' ? 'bg-[#A3CCA9]' : 'bg-[#CCCCCC]')}>Individual Ticket</div>
                         <div className={cn('flex-[0.333333] h-full flex items-center justify-center font-medium text-xs text-black text-center font-poppins', ticket.saleStatus === 'sold' ? 'bg-[#CCF5D2]' : 'bg-[#fff]')}>1</div>
-                        <div className={cn('flex-1 h-full flex items-center justify-center font-medium text-xs text-black text-center font-poppins rounded-tr-lg rounded-br-lg', eventEnded && 'flex-col gap-2', statusOptions[ticket.saleStatus!].bg, statusOptions[ticket.saleStatus!].color)}>
+                        <div className={cn('flex-1 h-full flex items-center justify-center font-medium text-xs text-black text-center font-poppins rounded-tr-lg rounded-br-lg', eventEnded && 'flex-col gap-2', statusOptions[(ticket as TicketType).saleStatus!].bg, statusOptions[(ticket as TicketType).saleStatus!].color)}>
                             {eventEnded && <p className='font-medium text-sm font-poppins text-[#ff0000]'>Event Ended</p>}
-                            {(!eventEnded || ticket.saleStatus === 'inEscrow') && statusOptions[ticket.saleStatus!].text}
+                            {(!eventEnded || (ticket as TicketType).saleStatus === 'inEscrow') && statusOptions[(ticket as TicketType).saleStatus!].text}
                         </div>
                     </>
                 ) : isBundleCallBack(ticket) ? (
@@ -127,14 +127,14 @@ export default function TicketRow({ event, ticket, bundleTickets }: Props)
                                 className='object-cover w-[62px] h-[60px] rounded-lg' 
                                 quality={100}
                             />
-                            <p className={cn('font-medium text-black flex flex-1 text-center h-full items-center justify-center', ticket.status === 'sold' ? 'bg-[#A3CCA9]' : 'bg-[#CCCCCC]')}>{event?.name}</p> 
+                            <p className={cn('font-medium text-black flex flex-1 text-center h-full items-center justify-center', (ticket as Bundle).status === 'sold' ? 'bg-[#A3CCA9]' : 'bg-[#CCCCCC]')}>{event?.name}</p> 
                         </div>
-                        <div className={cn('flex-1 h-full flex items-center justify-center font-medium text-xs text-black text-center font-poppins', ticket.status === 'sold' ? 'bg-[#CCF5D2]' : 'bg-[#fff]')}>Bundle Tickets</div>
-                        <div className={cn('flex-1 h-full flex items-center justify-center font-medium text-xs text-black text-center font-poppins', ticket.status === 'sold' ? 'bg-[#A3CCA9]' : 'bg-[#CCCCCC]')}>Bundle</div>
-                        <div className={cn('flex-[0.333333] h-full flex items-center justify-center font-medium text-xs text-black text-center font-poppins', ticket.status === 'sold' ? 'bg-[#CCF5D2]' : 'bg-[#fff]')}>{ticket.tickets.length}</div>
-                        <div className={cn('flex-1 h-full flex items-center justify-center font-medium text-xs text-black text-center font-poppins rounded-tr-lg rounded-br-lg', eventEnded && 'flex-col gap-2', statusOptions[ticket.status!].bg, statusOptions[ticket.status!].color)}>
+                        <div className={cn('flex-1 h-full flex items-center justify-center font-medium text-xs text-black text-center font-poppins', (ticket as Bundle).status === 'sold' ? 'bg-[#CCF5D2]' : 'bg-[#fff]')}>Bundle Tickets</div>
+                        <div className={cn('flex-1 h-full flex items-center justify-center font-medium text-xs text-black text-center font-poppins', (ticket as Bundle).status === 'sold' ? 'bg-[#A3CCA9]' : 'bg-[#CCCCCC]')}>Bundle</div>
+                        <div className={cn('flex-[0.333333] h-full flex items-center justify-center font-medium text-xs text-black text-center font-poppins', (ticket as Bundle).status === 'sold' ? 'bg-[#CCF5D2]' : 'bg-[#fff]')}>{(ticket as Bundle).tickets.length}</div>
+                        <div className={cn('flex-1 h-full flex items-center justify-center font-medium text-xs text-black text-center font-poppins rounded-tr-lg rounded-br-lg', eventEnded && 'flex-col gap-2', statusOptions[(ticket as Bundle).status!].bg, statusOptions[(ticket as Bundle).status!].color)}>
                             {eventEnded && <p className='font-medium text-sm font-poppins text-[#ff0000]'>Event Ended</p>}
-                            {(!eventEnded || ticket.status === 'inEscrow') &&statusOptions[ticket.status!].text}
+                            {(!eventEnded || (ticket as Bundle).status === 'inEscrow') &&statusOptions[(ticket as Bundle).status!].text}
                         </div>
                     </>
                 ) : null}
@@ -142,35 +142,35 @@ export default function TicketRow({ event, ticket, bundleTickets }: Props)
             <Dialog open={ticketOpen} onOpenChange={setTicketOpen}>
                 <DialogContent className='flex flex-col gap-6 w-full border-none outline-none py-12 z-[9999999999999999]'>
                     <DialogHeader className='flex items-center justify-center'>
-                        <DialogTitle className={cn('font-medium font-poppins text-center', ((isTicketCallBack(ticket) && ticket.saleStatus === 'cancelled') || (isBundleCallBack(ticket) && ticket.status === 'cancelled')) && 'text-[#ff0000]')}>
-                            {isTicketCallBack(ticket) && Object.keys(ticket.tickets)[0]} {isTicketCallBack(ticket) ? "Ticket" : "Bundle"} for {event.name} {((isTicketCallBack(ticket) && ticket.saleStatus === 'cancelled') || (isBundleCallBack(ticket) && ticket.status === 'cancelled')) && 'Rejected'}
-                            {((isTicketCallBack(ticket) && ticket.saleStatus === 'pending') || (isBundleCallBack(ticket) && ticket.status === 'pending')) && <span className='text-[#ff0000] w-full !text-center mt-2 !mx-auto'><br/><br/>Pending</span>}
+                        <DialogTitle className={cn('font-medium font-poppins text-center', ((isTicketCallBack(ticket) && (ticket as TicketType).saleStatus === 'cancelled') || (isBundleCallBack(ticket) && (ticket as Bundle).status === 'cancelled')) && 'text-[#ff0000]')}>
+                            {isTicketCallBack(ticket) && Object.keys((ticket as TicketType).tickets)[0]} {isTicketCallBack(ticket) ? "Ticket" : "Bundle"} for {event.name} {((isTicketCallBack(ticket) && (ticket as TicketType).saleStatus === 'cancelled') || (isBundleCallBack(ticket) && (ticket as Bundle).status === 'cancelled')) && 'Rejected'}
+                            {((isTicketCallBack(ticket) && (ticket as TicketType).saleStatus === 'pending') || (isBundleCallBack(ticket) && (ticket as Bundle).status === 'pending')) && <span className='text-[#ff0000] w-full !text-center mt-2 !mx-auto'><br/><br/>Pending</span>}
                         </DialogTitle>
                     </DialogHeader>
                     {isTicketCallBack(ticket) ? (
-                        <div className={cn('flex gap-2 w-full items-center justify-between bg-black px-4 py-2', eventEnded && (ticket.saleStatus !== 'inEscrow' && ticket.saleStatus !== 'sold') && 'opacity-40')}>
+                        <div className={cn('flex gap-2 w-full items-center justify-between bg-black px-4 py-2', eventEnded && ((ticket as TicketType).saleStatus !== 'inEscrow' && (ticket as TicketType).saleStatus !== 'sold') && 'opacity-40')}>
                             <div className='flex gap-2 items-center justify-center'>
-                                <div className={cn('w-6 h-6 rounded-full', ticket.saleStatus === 'pending' ? 'bg-[#D9D9D9]' : 'bg-gradient-to-b from-[#E72377] from-[-5.87%] to-[#EB5E1B] to-[101.65%]')} />
+                                <div className={cn('w-6 h-6 rounded-full', (ticket as TicketType).saleStatus === 'pending' ? 'bg-[#D9D9D9]' : 'bg-gradient-to-b from-[#E72377] from-[-5.87%] to-[#EB5E1B] to-[101.65%]')} />
                                 <p className='font-medium text-sm font-poppins text-white'>
-                                    {Object.keys(ticket.tickets)[0]}{" "}
-                                    {(Object.keys(ticket.seats).length > 0) && <span className='text-[#00CB20]'>R{Object.keys(ticket.seats)[0].split("_")[1]?.split("-")[1]} S{Object.keys(ticket.seats)[0].split("_")[2]?.split("-")[1]}</span>}
+                                    {Object.keys((ticket as TicketType).tickets)[0]}{" "}
+                                    {(Object.keys((ticket as TicketType).seats).length > 0) && <span className='text-[#00CB20]'>R{Object.keys((ticket as TicketType).seats)[0].split("_")[1]?.split("-")[1]} S{Object.keys((ticket as TicketType).seats)[0].split("_")[2]?.split("-")[1]}</span>}
                                 </p>
                             </div>
                             <div className='flex gap-2 items-center'>
                                 <div className='p-1 bg-[#55555580] rounded-[4px] flex items-center justify-center text-center'>
                                     <input autoFocus={false} value={price} onChange={(e) => (/^-?\d*\.?\d+$/.test(e.target.value) || e.target.value === '') ? setPrice(e.target.value) : setPrice(prev => prev)} placeholder="0.00" className='text-center text-white font-poppins text-sm font-normal bg-transparent outline-none w-[3.5rem]' />
-                                    <p className='font-medium text-sm font-poppins text-white'>{ticket.country}</p>
+                                    <p className='font-medium text-sm font-poppins text-white'>{(ticket as TicketType).country}</p>
                                 </div>
-                                {(typeof price === 'string' ? parseFloat(price) : price) !== ticket.salePrice && <div className='cursor-pointer rounded-full w-5 h-5 bg-white flex items-center justify-center' onClick={() => handleUpdatePrice(typeof price === 'string' ? parseFloat(price) : price!)}><Check size={16} /></div>}
+                                {(typeof price === 'string' ? parseFloat(price) : price) !== (ticket as TicketType).salePrice && <div className='cursor-pointer rounded-full w-5 h-5 bg-white flex items-center justify-center' onClick={() => handleUpdatePrice(typeof price === 'string' ? parseFloat(price) : price!)}><Check size={16} /></div>}
                             </div>
-                            {(ticket.saleStatus !== 'inEscrow' && ticket.saleStatus !== 'sold' && ticket.saleStatus !== 'cancelled' && !eventEnded) && <p onMouseDown={handleDeleteTicket} className='font-medium text-xs underline font-poppins cursor-pointer text-white'>Delete Ticket</p>} 
-                            {ticket.saleStatus === 'inEscrow' && <p className='text-xs font-medium font-poppins text-[#7061A3]'>In Escrow</p>}
-                            {ticket.saleStatus === 'sold' && <p className='text-xs font-medium font-poppins text-[#FF0000]'>Sold</p>}
-                            {eventEnded && (ticket.saleStatus !== 'inEscrow' && ticket.saleStatus !== 'sold') && <p className='text-xs font-medium font-poppins text-[#ff0000]'>Event Ended</p>}
+                            {((ticket as TicketType).saleStatus !== 'inEscrow' && (ticket as TicketType).saleStatus !== 'sold' && (ticket as TicketType).saleStatus !== 'cancelled' && !eventEnded) && <p onMouseDown={handleDeleteTicket} className='font-medium text-xs underline font-poppins cursor-pointer text-white'>Delete Ticket</p>} 
+                            {(ticket as TicketType).saleStatus === 'inEscrow' && <p className='text-xs font-medium font-poppins text-[#7061A3]'>In Escrow</p>}
+                            {(ticket as TicketType).saleStatus === 'sold' && <p className='text-xs font-medium font-poppins text-[#FF0000]'>Sold</p>}
+                            {eventEnded && ((ticket as TicketType).saleStatus !== 'inEscrow' && (ticket as TicketType).saleStatus !== 'sold') && <p className='text-xs font-medium font-poppins text-[#ff0000]'>Event Ended</p>}
                         </div>
                     ) : isBundleCallBack(ticket) ? (
                         <div className='flex gap-2 w-full flex-col'>
-                            <div className={cn('flex gap-2 w-full items-center justify-between bg-black px-4 py-2', eventEnded && (ticket.status !== 'inEscrow' && ticket.status !== 'sold') && 'opacity-40')}>
+                            <div className={cn('flex gap-2 w-full items-center justify-between bg-black px-4 py-2', eventEnded && ((ticket as Bundle).status !== 'inEscrow' && (ticket as Bundle).status !== 'sold') && 'opacity-40')}>
                                 <p className='font-medium text-sm font-poppins text-white'>
                                     Bundle Price
                                 </p>
@@ -179,11 +179,11 @@ export default function TicketRow({ event, ticket, bundleTickets }: Props)
                                         <input autoFocus={false} value={price} onChange={(e) => (/^-?\d*\.?\d+$/.test(e.target.value) || e.target.value === '') ? setPrice(e.target.value) : setPrice(prev => prev)} placeholder="0.00" className='text-center text-white font-poppins text-sm font-normal bg-transparent outline-none w-[3.5rem]' />
                                         <p className='font-medium text-sm font-poppins text-white'>{bundleTickets && bundleTickets[0].country}</p>
                                     </div>
-                                    {(typeof price === 'string' ? parseFloat(price) : price) !== ticket.price && <div className='cursor-pointer rounded-full w-5 h-5 bg-white flex items-center justify-center' onClick={() => handleUpdatePriceBundle(typeof price === 'string' ? parseFloat(price) : price!)}><Check size={16} /></div>}
+                                    {(typeof price === 'string' ? parseFloat(price) : price) !== (ticket as Bundle).price && <div className='cursor-pointer rounded-full w-5 h-5 bg-white flex items-center justify-center' onClick={() => handleUpdatePriceBundle(typeof price === 'string' ? parseFloat(price) : price!)}><Check size={16} /></div>}
                                 </div>
                             </div>
 
-                            {bundleTickets?.map(bundleTicket => (
+                            {(bundleTickets as TicketType[])?.map(bundleTicket => (
                                 <div key={bundleTicket.id} className={cn('flex gap-2 w-full items-center justify-between bg-black px-4 py-2', eventEnded && (ticket.status !== 'inEscrow' && ticket.status !== 'sold') && 'opacity-40')}>
                                     <div className='flex gap-2 items-center justify-center'>
                                         <div className={cn('w-6 h-6 rounded-full', ticket.status === 'pending' ? 'bg-[#D9D9D9]' : 'bg-gradient-to-b from-[#E72377] from-[-5.87%] to-[#EB5E1B] to-[101.65%]')} />
